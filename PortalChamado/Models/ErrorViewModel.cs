516c8c636e42d906b6830185645d1896cf2067c2
Esther Leão <esther.biasantos@gmail.com>
2020-03-12 00:56:58 -0300

    Project created

---
+++ PortalChamado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortalChamado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
