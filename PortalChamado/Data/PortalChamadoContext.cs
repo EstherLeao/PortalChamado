d9c1789ec200905edfb8cd8c5db04a06a9e75ce5
Esther Leão <esther.biasantos@gmail.com>
2020-03-14 18:23:47 -0300

    CRUD scaffolding

---
+++ PortalChamado/Data/PortalChamadoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortalChamado.Models;
+
+namespace PortalChamado.Data
+{
+    public class PortalChamadoContext : DbContext
+    {
+        public PortalChamadoContext (DbContextOptions<PortalChamadoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PortalChamado.Models.Acesso> Acesso { get; set; }
+    }
+}
--- PortalChamado/Models/Acesso.cs
+++ PortalChamado/Models/Acesso.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace PortalChamado.Models
 {
     public class Acesso
     {
+        [Key]
         public int IdAcesso { get; set; }
         public string TipoAcesso { get; set; }
     }
